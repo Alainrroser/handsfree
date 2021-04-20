daf4a2e820b83387705c85e595e60fc5aa20b729
bvonwm <marino.vonwattenwyl@bbcag.ch>
2021-04-20 13:13:01 +0200

    Add Tobii cursor test

--- src/main/java/ch/bbcag/handsfree/Launcher.java
+++ src/main/java/ch/bbcag/handsfree/Launcher.java
@@ -1,11 +1,32 @@
 package ch.bbcag.handsfree;
 
 import javafx.application.Application;
+import tobii.Tobii;
+
+import java.awt.*;
 
 public class Launcher {
 
-    public static void main(String[] args) {
-        Application.launch(HandsFreeApplication.class);
+    public static void main(String[] args) throws Exception {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int screenWidth = screenSize.width;
+        int screenHeight = screenSize.height;
+
+        Robot robot = new Robot();
+
+        while(true) {
+            float[] position = Tobii.gazePosition();
+            float xRelative = position[0];
+            float yRelative = position[1];
+
+            int x = (int) (xRelative * screenWidth);
+            int y = (int) (yRelative * screenHeight);
+            robot.mouseMove(x, y);
+
+            Thread.sleep(50);
+        }
+
+//        Application.launch(HandsFreeApplication.class);
     }
 
 }
