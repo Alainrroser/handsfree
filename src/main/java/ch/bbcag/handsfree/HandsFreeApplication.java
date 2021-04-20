f681cb7c25e1954abe37008197069d6cecbe79b9
bvonwm <marino.vonwattenwyl@bbcag.ch>
2021-04-20 09:51:10 +0200

    Create basic project structure

---
+++ src/main/java/ch/bbcag/handsfree/HandsFreeApplication.java
@@ -0,0 +1,14 @@
+package ch.bbcag.handsfree;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class HandsFreeApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("HandsFree");
+        primaryStage.show();
+    }
+
+}
---
+++ src/main/java/ch/bbcag/handsfree/Launcher.java
@@ -0,0 +1,11 @@
+package ch.bbcag.handsfree;
+
+import javafx.application.Application;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        Application.launch(HandsFreeApplication.class);
+    }
+
+}
