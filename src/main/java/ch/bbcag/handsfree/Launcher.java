1f860a7048b808bb3cec215b8d6276ab22bbf412
brosea-bbcag <brosea@bbcag.ch>
2021-04-22 11:38:50 +0200

    Added right click
    You can now right click if you blink more than one second

--- src/main/java/ch/bbcag/handsfree/Launcher.java
+++ src/main/java/ch/bbcag/handsfree/Launcher.java
@@ -6,8 +6,8 @@ public class Launcher {
 
     public static void main(String[] args) throws Exception {
 //        new SpeechTest().start();
-//        new TobiiTest().start();
-        Application.launch(HandsFreeApplication.class);
+        new TobiiTest().start();
+//        Application.launch(HandsFreeApplication.class);
     }
 
 }
--- src/main/java/ch/bbcag/handsfree/TobiiTest.java
+++ src/main/java/ch/bbcag/handsfree/TobiiTest.java
@@ -6,48 +6,50 @@ import java.awt.*;
 import java.awt.event.InputEvent;
 
 public class TobiiTest {
-
     public void start() throws Exception {
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         int screenWidth = screenSize.width;
         int screenHeight = screenSize.height;
-
+        
         Robot robot = new Robot();
         
         boolean pressed = false;
-        long startTime = System.currentTimeMillis();
-        long endTime = 0;
+        int timesBlinked = 0;
+        long startTime = 0;
+        long endTime;
         
         while(true) {
-            
             float[] position = Tobii.gazePosition();
             float xRelative = position[0];
             float yRelative = position[1];
-
+            
             int x = (int) (xRelative * screenWidth);
             int y = (int) (yRelative * screenHeight);
-
+            
             if(Tobii.isLeftEyePresent() && Tobii.isRightEyePresent()) { // Both eyes are open
                 endTime = System.currentTimeMillis();
                 robot.mouseMove(x, y);
                 if(pressed) {
                     pressed = false;
+                    timesBlinked++;
                 }
-                if((endTime - startTime) >= 1000) {
-                    robot.mousePress(InputEvent.BUTTON3_DOWN_MASK);
-                    robot.mouseRelease(InputEvent.BUTTON3_DOWN_MASK);
-                    pressed = true;
+                if(endTime - startTime >= 1000) {
+                    if(timesBlinked == 1) {
+                        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+                        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+                    }
+                    if(timesBlinked == 2) {
+                        robot.mousePress(InputEvent.BUTTON3_DOWN_MASK);
+                        robot.mouseRelease(InputEvent.BUTTON3_DOWN_MASK);
+                    }
+                    timesBlinked = 0;
                 }
             } else if(!Tobii.isLeftEyePresent() && !Tobii.isRightEyePresent()) { // Both eyes are closed
-                startTime = System.currentTimeMillis();
-                if(!pressed && ((endTime - startTime) <= 1000)) {
-                    robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-                    robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-                    pressed = true;
+                if(timesBlinked == 0) {
+                    startTime = System.currentTimeMillis();
                 }
+                pressed = true;
             }
-            Thread.sleep(10);
         }
     }
-    
 }
