38a155682c9d4b127201536f81850e73b534a105
bvonwm <marino.vonwattenwyl@bbcag.ch>
2021-04-28 16:05:34 +0200

    Add journal and back button and change the window size

--- src/main/java/ch/bbcag/handsfree/Const.java
+++ src/main/java/ch/bbcag/handsfree/Const.java
@@ -1,8 +1,8 @@
 package ch.bbcag.handsfree;
 
 public class Const {
-    public static final double WIDTH = 700;
-    public static final double HEIGHT = 500;
+    public static final double WIDTH = 900;
+    public static final double HEIGHT = 600;
     
     public static final int V_BOX_SPACING = 20;
     public static final int V_BOX_PADDING_TOP_BOTTOM = 50;
