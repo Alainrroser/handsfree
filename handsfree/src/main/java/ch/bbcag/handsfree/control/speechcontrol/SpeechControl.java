fdb9f97a73c0a5424cd8ebd4e0e563f86339c087
brosea-bbcag <brosea@bbcag.ch>
2021-05-07 15:43:00 +0200

    Added daily journal
    Added today's journal and cleaned up code

--- handsfree/src/main/java/ch/bbcag/handsfree/control/speechcontrol/SpeechControl.java
+++ handsfree/src/main/java/ch/bbcag/handsfree/control/speechcontrol/SpeechControl.java
@@ -9,74 +9,81 @@ import java.awt.event.KeyEvent;
 
 public class SpeechControl {
     
+    private HandsFreeContext context;
+    
     public SpeechControl(HandsFreeContext context) {
+        this.context = context;
         HandsFreeRobot robot = context.getRobot();
         
         String press = "press";
         context.getSpeechRecognizer().addListener(press, () -> robot.mousePress(InputEvent.BUTTON1_DOWN_MASK));
-        context.getSpeechRecognizer().addCommand(press);
+        addCommandToList(press);
     
         String release = "release";
         context.getSpeechRecognizer().addListener(release, () -> robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK));
-        context.getSpeechRecognizer().addCommand(release);
+        addCommandToList(release);
     
         String left = "left";
         context.getSpeechRecognizer().addListener(left, () -> robot.keyTypeSpecial(KeyEvent.VK_LEFT));
-        context.getSpeechRecognizer().addCommand(left);
+        addCommandToList(left);
     
         String up = "up";
         context.getSpeechRecognizer().addListener(up, () -> robot.keyTypeSpecial(KeyEvent.VK_UP));
-        context.getSpeechRecognizer().addCommand(up);
+        addCommandToList(up);
         
         String right = "right";
         context.getSpeechRecognizer().addListener(right, () -> robot.keyTypeSpecial(KeyEvent.VK_RIGHT));
-        context.getSpeechRecognizer().addCommand(right);
+        addCommandToList(right);
         
         String down = "down";
         context.getSpeechRecognizer().addListener(down, () -> robot.keyTypeSpecial(KeyEvent.VK_DOWN));
-        context.getSpeechRecognizer().addCommand(down);
+        addCommandToList(down);
         
         String scrollDown = "scroll down";
         context.getSpeechRecognizer().addListener(scrollDown, () -> robot.scrollContinuously(10));
-        context.getSpeechRecognizer().addCommand(scrollDown);
+        addCommandToList(scrollDown);
         
         String scrollUp = "scroll up";
         context.getSpeechRecognizer().addListener(scrollUp, () -> robot.scrollContinuously(-10));
-        context.getSpeechRecognizer().addCommand(scrollUp);
+        addCommandToList(scrollUp);
         
         String newTab = "new tab";
         context.getSpeechRecognizer().addListener(newTab, () -> robot.pressHotkey(KeyEvent.VK_CONTROL, KeyEvent.VK_T));
-        context.getSpeechRecognizer().addCommand(newTab);
+        addCommandToList(newTab);
         
         String closeTab = "close tab";
         context.getSpeechRecognizer().addListener(closeTab, () -> robot.pressHotkey(KeyEvent.VK_CONTROL, KeyEvent.VK_W));
-        context.getSpeechRecognizer().addCommand(closeTab);
+        addCommandToList(closeTab);
         
         String taskManager = "task manager";
         context.getSpeechRecognizer().addListener(taskManager, () -> robot.pressHotkey(KeyEvent.VK_CONTROL, KeyEvent.VK_SHIFT, KeyEvent.VK_ESCAPE));
-        context.getSpeechRecognizer().addCommand(taskManager);
+        addCommandToList(taskManager);
         
         String close = "close";
         context.getSpeechRecognizer().addListener(close, () -> robot.pressHotkey(KeyEvent.VK_ALT, KeyEvent.VK_F4));
-        context.getSpeechRecognizer().addCommand(close);
+        addCommandToList(close);
         
         String switchOpen = "switch open";
         context.getSpeechRecognizer().addListener(switchOpen, () -> {
             robot.keyPressSpecial(KeyEvent.VK_ALT);
             robot.keyPressSpecial(KeyEvent.VK_TAB);
         });
-        context.getSpeechRecognizer().addCommand(switchOpen);
+        addCommandToList(switchOpen);
         
         String switchClose = "switch close";
         context.getSpeechRecognizer().addListener(switchClose, () -> {
             robot.keyReleaseSpecial(KeyEvent.VK_ALT);
             robot.keyReleaseSpecial(KeyEvent.VK_TAB);
         });
-        context.getSpeechRecognizer().addCommand(switchClose);
+        addCommandToList(switchClose);
         
         for(Shortcut shortcut : context.getShortcutManager().getShortcuts()) {
             context.getSpeechRecognizer().addListener(shortcut.getName(), () -> context.getShortcutManager().runShortcut(shortcut.getName()));
         }
+    }
+    
+    private void addCommandToList(String command) {
+        context.getSpeechRecognizer().addCommand(command);
     }
 
 }
